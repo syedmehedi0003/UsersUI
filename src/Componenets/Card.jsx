b69a0b0627dba42c1921f5444dc77253b3606458
Syed Mahade <mehedihasanbahar@gmail.com>
2024-02-22 14:42:07 +0600

    new commit

--- src/Componenets/Card.jsx
+++ src/Componenets/Card.jsx
@@ -1,31 +1,15 @@
-import React from 'react';
+
 import { Link } from 'react-router-dom';
 
 
 function Card({ user }) {
-  // const [loading, setLoading] = useState(false);
 
-  // useEffect(() => {
-  //   setLoading(true);
-  //   fetch('https://dummyjson.com/users')
-  //     .then(res => res.json())
-  //     .then(data => {
-  //       setLoading(false)
-  //       setUsers(data.users)
-  //     });
-
-  // }, [])
 
   return (
 
     <div>
       <div className="border border-2 border-gray-500 rounded-md bg-gray-200">
-      {/* {
-        loading ?
-          <div>
-            <p>Loading.....</p>
-          </div> :
-          <> */}
+     
         <img className='w-1/4 mx-auto my-4 border border-2 border-gray-500 rounded-full p-2' src={user.image} alt="image" />
        
         <div className=' text-left pl-4 pb-3'>      
@@ -33,9 +17,7 @@ function Card({ user }) {
           <p><span className='font-bold'>Email: </span>{user.email}</p>
           <p><span className='font-bold'>Address: </span>{user.address.address}</p>
           <p><span className='font-bold'>Company Name: </span>{user.company.name}</p></div>
-          {/* </>
-
-      } */}
+    
       </div>
     </div>
   )
